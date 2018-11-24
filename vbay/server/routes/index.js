532d4643702c8052f6002ecca8be4e8681a46e34
Jason Kirshner <jlk2918@g.rit.edu>
2018-11-24 14:57:12 -0500

    Added CRUD routes for each table

--- vbay/server/routes/index.js
+++ vbay/server/routes/index.js
@@ -1,34 +1,94 @@
-const usersController = require('../controllers').users;
-const gamesController = require('../controllers').games;
+const auctionsController = require('../controllers').auctions
+const bidsController = require('../controllers').bids
+const completedtradesController = require('../controllers').completedtrades
+const gamesController = require('../controllers').games
+const offersController = require('../controllers').offers
+const salesController = require('../controllers').sales
+const tradesController = require('../controllers').trades
+const usersController = require('../controllers').users
 
 module.exports = (app) => {
-    app.get('/api', (req, res) => res.status(200).send({
+    app.get('/vbay-api', (req, res) => res.status(200).send({
         message: 'You have successfully connected to the vbay API',
-    }));
+    }))
 
-    /* Creates */
-    //Users
-    app.post('/api/users', usersController.create);
-    //Games
-    app.post('/api/games', gamesController.create);
+    /**** CREATE ****/
+    // Auctions
+    app.post('/vbay-api/auctions', auctionsController.create)
+    // Bids
+    app.post('/vbay-api/bids', bidsController.create)
+    // CompletedTrades
+    app.post('/vbay-api/completedtrades', completedtradesController.create)
+    // Games
+    app.post('/vbay-api/games', gamesController.create)
+    // Offers
+    app.post('/vbay-api/offers', offersController.create)
+    // Sales
+    app.post('/vbay-api/sales', salesController.create)
+    // Trades
+    app.post('/vbay-api/trades', tradesController.create)
+    // Users
+    app.post('/vbay-api/users', usersController.create)
 
-    /* Reads */
-    //Users
-    app.get('/api/users', usersController.list);
-    app.get('/api/users/:id', usersController.retrieve);
-    //Games
-    app.get('/api/games', gamesController.list);
-    app.get('/api/games/:gameid', gamesController.retrieve);
+    /**** READ ****/
+    // Auctions
+    app.get('/vbay-api/auctions', auctionsController.list)
+    app.get('/vbay-api/auctions/:auctionid', auctionsController.retrieve)
+    // Bids
+    app.get('/vbay-api/bids', bidsController.list)
+    app.get('/vbay-api/bids/:bidid', bidsController.retrieve)
+    // CompletedTrades
+    app.get('/vbay-api/completedtrades', completedtradesController.list)
+    app.get('/vbay-api/completedtrades/:completionid', completedtradesController.retrieve)
+    // Games
+    app.get('/vbay-api/games', gamesController.list)
+    app.get('/vbay-api/games/:gameid', gamesController.retrieve)
+    // Offers
+    app.get('/vbay-api/offers', offersController.list)
+    app.get('/vbay-api/offers/:offerid', offersController.retrieve)
+    // Sales
+    app.get('/vbay-api/sales', salesController.list)
+    app.get('/vbay-api/sales/:saleid', salesController.retrieve)
+    // Trades
+    app.get('/vbay-api/trades', tradesController.list)
+    app.get('/vbay-api/trades/:tradeid', tradesController.retrieve)
+    // Users
+    app.get('/vbay-api/users', usersController.list)
+    app.get('/vbay-api/users/:id', usersController.retrieve)
 
-    /* Updates */
-    //Users
-    app.put('/api/users/:id', usersController.update);
-    //Games
-    app.put('/api/games/:gameid', gamesController.update);
+    /**** UPDATE ****/
+    // Auctions
+    app.put('/vbay-api/auctions/:auctionid', auctionsController.update)
+    // Bids
+    app.put('/vbay_api/bids/:bidid', bidsController.update)
+    // CompletedTrades
+    app.put('/vbay_api/completedtrades/:completionid', completedtradesController.update)
+    // Games
+    app.put('/vbay-api/games/:gameid', gamesController.update)
+    // Offers
+    app.put('/vbay_api/offers/:offerid', offersController.update)
+    // Sales
+    app.put('/vbay_api/sales/:saleid', salesController.update)
+    // Trades
+    app.put('/vbay_api/trades/:tradeid', tradesController.update)
+    // Users
+    app.put('/vbay-api/users/:id', usersController.update)
 
-    /* Deletes */
-    //Users
-    app.delete('/api/users/:id', usersController.destroy);
-    //Games
-    app.delete('/api/games/:gameid', gamesController.destroy);
-};
+    /**** DELETE ****/
+    // Auctions
+    app.delete('/vbay-api/auctions/:auctionid', auctionsController.destroy)
+    // Bids
+    app.delete('/vbay-api/bids/:bidid', bidsController.destroy)
+    // CompletedTrades
+    app.delete('/vbay-api/completedtrades/:completionid', completedtradesController.destroy)
+    // Games
+    app.delete('/vbay-api/games/:gameid', gamesController.destroy)
+    // Offers
+    app.delete('/vbay-api/offers/:offerid', offerController.destroy)
+    // Sales
+    app.delete('/vbay-api/sales/:saleid', salesController.destroy)
+    // Trades
+    app.delete('/vbay-api/trades/:tradeid', tradesController.destroy)
+    // Users
+    app.delete('/vbay-api/users/:id', usersController.destroy)
+}
