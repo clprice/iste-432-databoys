385fe86a1d6ef8ba464dae922658cc2a29af7631
Gregory Ghiroli <31124595+GregoryGhiroli@users.noreply.github.com>
2018-12-09 23:46:48 -0500

    Add files via upload
    Final auction and index.php

--- Front-End Layer/index.php
+++ Front-End Layer/index.php
@@ -6,7 +6,8 @@
 
   <title>VBay</title>
   <link rel="stylesheet" href="style.css">
-   
+  <link rel="stylesheet" href="loader.css">
+ 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     
@@ -15,159 +16,138 @@
 
 
 
-//global vars
-/*
-      var games=[];
-    
-    
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://afternoon-beyond-89008.herokuapp.com/vbay-api/games/gameid",
-  "method": "GET",
-  "headers": {}
-}
 
-//first loop to collect url
-for ( var x = 1; x<10; x++){
-    var tmp = jQuery.extend(true, {}, settings);
-    tmp.url += x.toString();
-  
-  
-//ajax to collect everything    
-$.ajax(tmp).always(function (response) {
-    
-    if(response.name != null){
-    //get the nme of the games
-    var y = response.name;
-    //get the name of the games for looping with doc id
-    var name = '#games' + i;
-    //store in a global array
-    games.push(y);
-    //cehck
-   // console.log(games);
-    //does nothing
-   // $(name).html(y);
-    }
-    //loop every element in array
-      for(var i=1; i<10;i++){
-          //find the element of the id of games plus spot in array
-             var ap = document.getElementById("games"+i);
-          //check again
-           console.log(games);
-        //make sure nothing is null
-        if(games[i] != null){
-            //set the game to a
-            var a = games[i];
-            //get the conent of the element so no duplicates
-            var b = ap.textContent;
-            //unused x
-            var z;
-            //make sure the one were putting in isn't the same as the one thats in there
-            if(a != b){
-            //put it in
-        ap.append(games[i]);
-            }
-            else{
-                //make sure the next one is different so its not added in
-                b = a;
-            }
-        }
-        }//end of i for loop
-		
-});
-}//end of for urls 
-   
-  
-  */
+
 var settings = {
   "async": true,
   "crossDomain": true,
   "url": "https://afternoon-beyond-89008.herokuapp.com/vbay-api/games/",
   "method": "GET",
   "headers": {
-    
+  
   }
 }
 
 $.ajax(settings).done(function (response) {
-  console.log(response[0].name);
-  for(int i = 0; i < 10; i++;){
-	  
-	  var get = document.getElementById("games"+i);
-	  get.append(response[i].name);
-	  
+  console.log(response);
+  for(var i = 0; i <response.length ; i++){
+	  if(response[i].name !=null){
+	  console.log(response[i].name);
+	    var ap = document.getElementById("games"+i);
+        var gameNum = document.getElementById("imgSrc"+i);
+		var att = document.createAttribute("game-type"); 
+		att.value = response[i].gameid;
+		console.log(att);
+        gameNum.setAttributeNode(att);
+		ap.append(response[i].name);
+		gameNum.addEventListener("click", function() {
+		console.log(this.getAttribute("game-type"));
+		var leave = this.getAttribute("game-type");
+		  localStorage.setItem("id",leave);
+		   window.location.href = "https://people.rit.edu/gpg9583/vbay/auction.php";
+		
+});
+		
+	  }
   }
-});  
+});
+
+
+
+ var settings2 = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://afternoon-beyond-89008.herokuapp.com/vbay-api/auctions",
+  "method": "GET",
+  "headers": {
+    
+  }
+}
+
+
+$.ajax(settings2).done(function (response) {
+	
+		console.log(response[0].bids[0].price);
+	    var spot = ((response[0].bids).length) -1;
+		console.log(spot);
+		var ap = document.getElementById("price"+0);
+		ap.append(response[0].bids[spot].price);
+
+		
+});
     </script>
 </head>
 
 <body>
 
-<a href="index.html"><img src="VBay logo.PNG" alt="logo" height = "50px"></a>
-<?php include 'nav.php';?>
+
+<a href="index.php"><img src="VBay logo.PNG" alt="logo" height = "50px"></a>
+
+<?php include('nav.php'); ?>
     
     
+  
 <div class="grid-container">
-    
- 
+  
+
 <div id = <class="grid-item">
-<h2> Item Name:<p id="games1"></p></h2> 
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
-<h3> Item Price</h3>
+<h2> Item Name:<p id="games0"></p></h2> 
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/kqlntgss9yb5invq8nxi.jpg" height = "200px" width = "175px" id ='imgSrc0'></img>
+<h3> Item Price<p id="price0"></p> </h3>
+</div>
+<div id = <class="grid-item">
+<h2> Item Name:<p id="games1"></p></h2>
+<a href="auction.phpl"><img src = "https://images.igdb.com/igdb/image/upload/t_thumb/izjyh7la8knxfxfvwkos.jpg" height = "200px" width = "175px" id ='imgSrc1'></img></a>
+
+<h3> Item Price 20.00</h3>
 </div>
 <div id = <class="grid-item">
 <h2> Item Name:<p id="games2"></p></h2>
-<img src = "image2.png" height = "200px" width = "175px"></img>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/ahkeoao7n9ypscyfpelq.jpg" height = "200px" width = "175px" id ='imgSrc2'></img>
 
-<h3> Item Price</h3>
+<h3> Item Price 15.00</h3>
 </div>
 <div id = <class="grid-item">
 <h2> Item Name:<p id="games3"></p></h2>
-<img src = "image3.jpg" height = "200px" width = "175px"></img>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/obimr8uf45m5cdjltket.jpg" height = "200px" width = "175px"id ='imgSrc3'></img>
 
 <h3> Item Price</h3>
 </div>
 <div id = <class="grid-item">
 <h2> Item Name:<p id="games4"></p></h2>
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/lilz0thlfxutzvsbttf6.jpg" height = "200px" width = "175px"id ='imgSrc4'></img>
 
-<h3> Item Price</h3>
+<h3> Item Price 20.00</h3>
 </div>
 <div id = <class="grid-item">
-<h2> Item Name:<p id="games5"></p></h2>
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
+<h2> Item Name <p id="games5"></p></h2>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/ittzyszzkfreqqyp4e0l.jpg" height = "200px" width = "175px"id ='imgSrc5'></img>
 
-<h3> Item Price</h3>
+<h3> Item Price 23.00</h3>
 </div>
 <div id = <class="grid-item">
-<h2> Item Name <p id="games6"></p></h2>
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
+<h2> Item Name: <p id="games6"></p></h2>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/nddlcagqy6cuqwm1rrdj.jpg" height = "200px" width = "175px"id ='imgSrc6'></img>
 
-<h3> Item Price</h3>
+<h3> Item Price35.00</h3>
 </div>
 <div id = <class="grid-item">
-<h2> Item Name:<p id="games7"></p></h2>
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
+<h2> Item Name: <p id="games7"></p></h2>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/oscbf1gxw6oqrevjzs0y.jpg" height = "200px" width = "175px"id ='imgSrc7'></img>
 
-<h3> Item Price</h3>
+<h3> Item Price 30.00</h3>
 </div>
 <div id = <class="grid-item">
-<h2> Item Name:<p id="games8"></p></h2>
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
+<h2> Item Name: <p id="games8"></p></h2>
+<img src = "https://images.igdb.com/igdb/image/upload/t_thumb/l0u3uqmnnj8ws9ysm67k.jpg" height = "200px" width = "175px"id ='imgSrc8'></img>
 
-<h3> Item Price</h3>
-</div>
-<div id = <class="grid-item">
-<h2> Item Name:<p id="games9"></p></h2>
-<img src = "image1.jpg" height = "200px" width = "175px"></img>
-
-<h3> Item Price</h3>
+<h3> Item Price 45.00</h3>
 </div>
 
 
 </div>
 
-<?php include 'foot.php';?>
+<?php include('foot.php'); ?>
+
 </body>
 </html>
